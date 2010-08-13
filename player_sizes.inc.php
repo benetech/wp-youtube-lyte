18562890fc8363043c6e6106cf83962e8d4ee1d8
futtta <futtta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-13 13:39:12 +0000

    the cleaning up release
    git-svn-id: https://plugins.svn.wordpress.org/wp-youtube-lyte/tags/0.5.1@275373 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ player_sizes.inc.php
@@ -0,0 +1,33 @@
+<?php
+$pDefault=2;
+
+$pSize[0][a]=true;
+$pSize[0][w]=425;
+$pSize[0][h]=344;
+$pSize[0][t]="Smaller 4:3 player";
+
+$pSize[1][a]=true;
+$pSize[1][w]=560;
+$pSize[1][h]=340;
+$pSize[1][t]="Smaller 16:9 player";
+
+$pSize[2][a]=true;
+$pSize[2][w]=480;
+$pSize[2][h]=385;
+$pSize[2][t]="Standard value, YouTube default for 4:3-ratio video";
+
+$pSize[3][a]=true;
+$pSize[3][w]=640;
+$pSize[3][h]=385;
+$pSize[3][t]="YouTube default for 16:9-ratio video";
+
+$pSize[4][a]=true;
+$pSize[4][w]=640;
+$pSize[4][h]=505;
+$pSize[4][t]="Larger 4:3 player";
+
+$pSize[5][a]=true;
+$pSize[5][w]=853;
+$pSize[5][h]=505;
+$pSize[5][t]="Larger 16:9 player";
+?>
