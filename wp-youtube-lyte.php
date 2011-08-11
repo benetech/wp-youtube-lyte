1eef50d8101651f5521b51061c5b5bc2854e1d5b
futtta <futtta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-11 13:59:19 +0000

    youtu.be links should work as well
    git-svn-id: https://plugins.svn.wordpress.org/wp-youtube-lyte/tags/0.7.3@422163 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-youtube-lyte.php
+++ wp-youtube-lyte.php
@@ -2,57 +2,72 @@
 /*
 Plugin Name: WP YouTube Lyte
 Plugin URI: http://blog.futtta.be/tag/lyte
-Description: WordPress Lite YouTube Embeds (with optional HTML5 video) in posts.
+Description: Lite and accessible YouTube audio and video embedding.
 Author: Frank Goossens (futtta)
-Version: 0.6.5
+Version: 0.7.3
 Author URI: http://blog.futtta.be/
 */
+
+$plugin_dir = basename(dirname(__FILE__)).'/languages';
+load_plugin_textdomain( 'wp-youtube-lyte', null, $plugin_dir );
+
+$wp_lyte_plugin_url = defined('WP_PLUGIN_URL') ? trailingslashit(WP_PLUGIN_URL . '/' . dirname(plugin_basename(__FILE__))) : trailingslashit(get_bloginfo('wpurl')) . PLUGINDIR . '/' . dirname(plugin_basename(__FILE__));
 
 require_once(dirname(__FILE__).'/options.php');
 require_once(dirname(__FILE__).'/player_sizes.inc.php');
 require_once(dirname(__FILE__).'/widget.php');
 
-$wp_lyte_plugin_url = defined('WP_PLUGIN_URL') ? trailingslashit(WP_PLUGIN_URL . '/' . dirname(plugin_basename(__FILE__))) : trailingslashit(get_bloginfo('wpurl')) . PLUGINDIR . '/' . dirname(plugin_basename(__FILE__)); 
-
-$lyteSettings[0]=$wp_lyte_plugin_url."lyte/";
+$lyteSettings[0]=$wp_lyte_plugin_url.'lyte/';
 
 if (get_option('newTube')==="1") {
-        $lyteSettings[1]="lyte-newtube-min.js";
-} else {
-        $lyteSettings[1]="lyte-min.js";
+        $lyteSettings[1]="newtube-";
 }
 
 $oSize = (int) get_option('size');
 if ((is_bool($oSize)) || ($pSize[$oSize]['a']===false)) { $sel = (int) $pDefault; } else { $sel= $oSize; }
+
 $lyteSettings[2]=$pSize[$sel]['w'];
 $lyteSettings[3]=$pSize[$sel]['h'];
-
 $lyteSettings[4]=get_option('show_links');
 
 function lyte_parse($the_content) {
 	global $lyteSettings;
-	if(strpos($the_content, "httpv")!==FALSE  ) {
+	if((strpos($the_content, "httpv")!==FALSE)||(strpos($the_content, "httpa")!==FALSE)) {
 		$char_codes = array('&#215;','&#8211;');
 		$replacements = array("x", "--");
 		$the_content=str_replace($char_codes, $replacements, $the_content);
 	    
-		preg_match_all("/httpv:\/\/([a-zA-Z0-9\-\_]+\.|)youtube\.com\/watch(\?v\=|\/v\/)([a-zA-Z0-9\-\_]{11})([^<\s]*)/", $the_content, $matches, PREG_SET_ORDER); 
+		preg_match_all("/http(a|v):\/\/([a-zA-Z0-9\-\_]+\.|)(youtube|youtu)(\.com|\.be)\/(watch(\?v\=|\/v\/)|)([a-zA-Z0-9\-\_]{11})([^<\s]*)/", $the_content, $matches, PREG_SET_ORDER); 
+
 		foreach($matches as $match) {
+			if ($match[1]!=="a") {
+ 				$divHeight=$lyteSettings[3];
+                                $audioClass="";
+			} else {
+				$audioClass=" audio";
+				if ($lyteSettings[1]=="newtube-"){
+					$divHeight=33;
+				} else {
+					$divHeight=25;
+				}
+			}
+
 			switch ($lyteSettings[4]) {
 				case "0":
-					$noscript_post="<br />Watch on YouTube";
+					$noscript_post="<br />".__("Watch this video on YouTube","wp-youtube-lyte");
 					$lytelinks_txt="";
 					break;
         			case "2":
 				        $noscript_post="";
-					$lytelinks_txt="<div class=\"lL\">Watch this video <a href=\"http://youtu.be/".$match[3]."\">on YouTube</a> or on <a href=\"http://icant.co.uk/easy-youtube/?http://www.youtube.com/watch?v=".$match[3]."\">Easy Youtube</a>.</div>";
+					$lytelinks_txt="<div class=\"lL\">".__("Watch this video","wp-youtube-lyte")." <a href=\"http://youtu.be/".$match[7]."\">".__("on YouTube","wp-youtube-lyte")."</a> ".__("or on","wp-youtube-lyte")." <a href=\"http://icant.co.uk/easy-youtube/?http://www.youtube.com/watch?v=".$match[7]."\">Easy Youtube</a>.</div>";
 					break;
 				default:
                                         $noscript_post="";
-                                        $lytelinks_txt="<div class=\"lL\">Watch this video <a href=\"http://youtu.be/".$match[3]."\">on YouTube</a>.</div>";
+                                        $lytelinks_txt="<div class=\"lL\">".__("Watch this video","wp-youtube-lyte")." <a href=\"http://youtu.be/".$match[7]."\">".__("on YouTube","wp-youtube-lyte")."</a>.</div>";
 			}
-			$lytetemplate = "<div class=\"lyte\" id=\"".$match[3]."\" style=\"width:".$lyteSettings[2]."px;height:".$lyteSettings[3]."px;\"><noscript><a href=\"http://youtu.be/".$match[3]."\"><img src=\"http://img.youtube.com/vi/".$match[3]."/0.jpg\">".$noscript_post."</a></noscript><script type=\"text/javascript\"><!-- \n var bU='".$lyteSettings[0]."';var d=document;if(d.addEventListener){d.addEventListener('DOMContentLoaded', insert, false)}else{window.onload=insert} function insert(){if(!d.getElementById('lytescr')){lytescr=d.createElement('script');lytescr.async=true;lytescr.id='lytescr';lytescr.src='".$lyteSettings[0].$lyteSettings[1]."';h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(lytescr, h)}}; \n --></script></div>".$lytelinks_txt;
-			$the_content = preg_replace("/httpv:\/\/([a-zA-Z0-9\-\_]+\.|)youtube\.com\/watch(\?v\=|\/v\/)([a-zA-Z0-9\-\_]{11})([^\s<]*)/", $lytetemplate, $the_content, 1);
+
+			$lytetemplate = "<div class=\"lyte".$audioClass."\" id=\"".$match[7]."\" style=\"width:".$lyteSettings[2]."px;height:".$divHeight."px;\"><noscript><a href=\"http://youtu.be/".$match[7]."\"><img src=\"http://img.youtube.com/vi/".$match[7]."/0.jpg\" alt=\"\" width=\"".$lyteSettings[2]."\" height=\"".$divHeight."\" />".$noscript_post."</a></noscript><script type=\"text/javascript\"><!-- \n var nT='".$lyteSettings[1]."';var bU='".$lyteSettings[0]."';var d=document;if(d.addEventListener){d.addEventListener('DOMContentLoaded', insert, false)}else{window.onload=insert} function insert(){if(!d.getElementById('lytescr')){lytescr=d.createElement('script');lytescr.async=true;lytescr.id='lytescr';lytescr.src='".$lyteSettings[0]."lyte-min.js';h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(lytescr, h)}}; \n --></script></div>".$lytelinks_txt;
+			$the_content = preg_replace("/(<p>)?http(v|a):\/\/([a-zA-Z0-9\-\_]+\.|)(youtube|youtu)(\.com|\.be)\/(watch(\?v\=|\/v\/)|)([a-zA-Z0-9\-\_]{11})([^\s<]*)(<\/p>)?/", $lytetemplate, $the_content, 1);
 		}
 	}
     return $the_content;
@@ -62,6 +77,10 @@ add_filter('the_content', 'lyte_parse', 90);
 
 /* donottrack */ 
 $donottrack_js=$wp_lyte_plugin_url."external/donottrack-min.js";
+
+if ($_SERVER['HTTPS']) {
+       $donottrack_js = str_replace( $donottrack_js, "http:","https:" );
+       }
 
 function init_lyte_donottrack() {
 	global $donottrack_js;
