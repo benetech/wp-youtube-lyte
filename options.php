ac452c954a5aadf749e70170d44121f8d18e183a
futtta <futtta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-21 00:16:24 +0000

    bugfixing is good for you
    git-svn-id: https://plugins.svn.wordpress.org/wp-youtube-lyte/tags/1.1.8@601823 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -1,5 +1,6 @@
 <?php
-        $wp_lyte_plugin_url = defined('WP_PLUGIN_URL') ? trailingslashit(WP_PLUGIN_URL . '/' . dirname(plugin_basename(__FILE__))) : trailingslashit(get_bloginfo('wpurl')) . PLUGINDIR . '/' . dirname(plugin_basename(__FILE__));
+$plugin_dir = basename(dirname(__FILE__)).'/languages';
+load_plugin_textdomain( 'wp-youtube-lyte', false, $plugin_dir );
 
 add_action('admin_menu', 'lyte_create_menu');
 
@@ -11,83 +12,103 @@ function lyte_create_menu() {
 }
 
 function register_lyte_settings() {
-	register_setting( 'lyte-settings-group', 'newTube' );
 	register_setting( 'lyte-settings-group', 'show_links' );
 	register_setting( 'lyte-settings-group', 'size' );
-	register_setting( 'lyte-settings-group', 'donottrack' );
+	register_setting( 'lyte-settings-group', 'hidef' );
+	register_setting( 'lyte-settings-group', 'position' );
+	register_setting( 'lyte-settings-group', 'notification' );
 }
 
 function lyte_admin_scripts() {
-	global $wp_lyte_plugin_url;
-	wp_enqueue_script('jqzrssfeed',$wp_lyte_plugin_url.'external/jquery.zrssfeed.min.js',array(jquery),null,true);
-	wp_enqueue_script('jqcookie',$wp_lyte_plugin_url.'external/jquery.cookie.min.js',array(jquery),null,true);
+	wp_enqueue_script('jqzrssfeed', plugins_url('/external/jquery.zrssfeed.min.js', __FILE__), array(jquery),null,true);
+	wp_enqueue_script('jqcookie', plugins_url('/external/jquery.cookie.min.js', __FILE__), array(jquery),null,true);
 }
 
 function lyte_admin_styles() {
-        global $wp_lyte_plugin_url;
-        wp_enqueue_style('zrssfeed',$wp_lyte_plugin_url.'external/jquery.zrssfeed.css');
+        wp_enqueue_style('zrssfeed', plugins_url('/external/jquery.zrssfeed.css', __FILE__));
 }
+
+function lyte_admin_notice(){
+    echo '<div class="updated"><p>Hello WP YouTube Lyte user!<br />Just to let you know that <strong>the bonus feature, DoNotTrack, was removed</strong> from WP YouTube Lyte. If you would like to keep blocking third party tracking on your blog, you might want to <strong>install <a href="http://wordpress.org/extend/plugins/wp-donottrack/" title="WP DoNotTrack">WP DoNotTrack</a></strong>, which is a more powerful and flexible solution.</p><p>Have a great day!<br /><a href="http://blog.futtta.be/">frank</a>.</div>';
+    }
+
+if (get_option('notification','0')!=="2") {
+	add_action('admin_notices', 'lyte_admin_notice');
+	update_option('notification','2');
+	}
 
 function lyte_settings_page() {
 ?>
 <div class="wrap">
-<h2>WP YouTube Lyte Settings</h2>
+<h2><?php _e("WP YouTube Lyte Settings","wp-youtube-lyte") ?></h2>
 <div style="float:left;width:70%;">
-<p>WP-YouTube-Lyte inserts "Lite YouTube Embeds" in your blog. These look and feel like normal embedded YouTube, but don't use Flash unless clicked on, thereby <a href="http://blog.futtta.be/2010/08/30/the-state-of-wp-youtube-lyte/" target="_blank">reducing download size & rendering time substantially</a>. The HTML5-option even allows for entirely Flash-less YouTube embeds, using H264 or WebM to play the video in compatible browsers. You can find more info on the <a href="http://wordpress.org/extend/plugins/wp-youtube-lyte/" target="_blank">wordpress.org WP-YouTube-Lyte page</a>.</p>
-<p>You can place video in your posts and pages by adding one or more http<strong>v</strong> YouTube-links to your post. These will automatically be replaced by WP-YouTube-Lyte with the correct (flash-less) code. Just replace the "http://" in the link with "httpv://", like this:
-<blockquote>http<strong>v</strong>://www.youtube.com/watch?v=QQPSMRQnNlU</blockquote></p>
-<p>You can modify WP-YouTube-Lyte's behaviour by changing the following settings:</p>
+<p><?php _e("WP YouTube Lyte inserts \"Lite YouTube Embeds\" in your blog. These look and feel like normal embedded YouTube, but don't use Flash unless clicked on, thereby <a href=\"http://blog.futtta.be/2012/04/03/speed-matters-re-evaluating-wp-youtube-lytes-performance/\" target=\"_blank\">reducing download size & rendering time substantially</a>. When a video is played, WP-YouTube-Lyte can either activate <a href=\"http://apiblog.youtube.com/2010/07/new-way-to-embed-youtube-videos.html\" target=\"_blank\">YouTube's embedded html5-player</a> or the older Flash-version, depending on the settings below.","wp-youtube-lyte") ?></p>
+<p><?php _e("You can place video and audio in your posts and pages by adding one or more http<strong>v</strong> or http<strong>a</strong> YouTube-links to your post. These will automatically be replaced by WP YouTube Lyte with the correct (flash-less) code. To add a video for example, you type a URL like <em>http<strong>v</strong>://www.youtube.com/watch?v=QQPSMRQnNlU</em> or <em>http<strong>v</strong>://www.youtube.com/playlist?list=PLA486E741B25F8E00</em> for a playlist. If you want an audio-only player, you enter <em>http<strong>a</strong>://www.youtube.com/watch?v=BIQIGR-kWtc</em>. There's more info on the <a href=\"http://wordpress.org/extend/plugins/wp-youtube-lyte/faq/\" target=\"_blank\">wordpress.org WP YouTube Lyte FAQ page</a>.","wp-youtube-lyte") ?></p>
+<p><?php _e("You can modify WP-YouTube-Lyte's behaviour by changing the following settings:","wp-youtube-lyte") ?></p>
 <form method="post" action="options.php">
     <?php settings_fields( 'lyte-settings-group' ); ?>
     <table class="form-table">
-		<tr valign="top">
-			<th scope="row">Play video using Flash or HTML5-video?</th>
-			<td>
-				<fieldset><legend class="screen-reader-text"><span>Use Flash or HTML5 video?</span></legend>
-					<label title="embed HTML5 video (highly experimental)"><input type="radio" name="newTube" value="1" <?php if (get_option('newTube')==="1") echo "checked" ?> /> Embed HTML5 video (experimental, uses <a href="http://apiblog.youtube.com/2010/07/new-way-to-embed-youtube-videos.html" target="_blank">the brand new YouTube embed-code</a>)</label><br />
-                                        <label title="normal YouTube embeds with Flash video"><input type="radio" name="newTube" value="0" <?php if (get_option('newTube')!=="1") echo "checked" ?> /> Normal YouTube embeds with Flash video.</label><br />
-			</fieldset>
-			</td>
-        </tr>
+	<input type="hidden" name="notification" value="<?php echo get_option('notification','0'); ?>" />
+
         <tr valign="top">
             <th scope="row">Player size:</th>
             <td>
-                 <fieldset><legend class="screen-reader-text"><span>Player size</span></legend>
+                <fieldset><legend class="screen-reader-text"><span><?php _e("Player size","wp-youtube-lyte") ?></span></legend>
 		<?php require 'player_sizes.inc.php';
-			$i=0;
 			if (is_bool(get_option('size'))) { $sel = (int) $pDefault; } else { $sel= (int) get_option('size'); }
-		 	while ($pS=$pSize[$i]) { 
-				if ($pS[a]===true) {
-				?>
-				<label title="<?php echo $pS[w]."X".$pS[h]; ?>"><input type="radio" name="size" class="l_size" value="<?php echo $i."\"";if($i===$sel) echo " checked";echo " /> ".$pS[w]."X".$pS[h]." (".$pS[t];?>)</label><br />
-				<?php
+			foreach (array("169","43") as $f) {
+				foreach ($pSizeOrder[$f] as $i) {
+					$pS=$pSize[$i];
+					if ($pS[a]===true) {
+						?>
+						<label title="<?php echo $pS[w]."X".$pS[h]; ?>"><input type="radio" name="size" class="l_size" value="<?php echo $i."\"";if($i===$sel) echo " checked";echo " /> ".$pS[w]."X".$pS[h]." (".$pS[t];?>)</label><br />
+						<?php
+					}
 				}
-				$i++;
+				?><br /><?php
 			}
 		?>
                 </fieldset>
              </td>
          </tr>
         <tr valign="top">
-			<th scope="row">Add links below the embedded videos?</th>
+			<th scope="row"><?php _e("Add links below the embedded videos?","wp-youtube-lyte") ?></th>
 			<td>
-				<fieldset><legend class="screen-reader-text"><span>Show links?</span></legend>
-					<label title="Show YouTube-link"><input type="radio" name="show_links" value="1" <?php if (get_option('show_links')==="1") echo "checked" ?> /> Add YouTube-link.</label><br />
-					<label title="Show YouTube and Ease YouTube link"><input type="radio" name="show_links" value="2" <?php if (get_option('show_links')==="2") echo "checked" ?> /> Add both a YouTube and an <a href="http://icant.co.uk/easy-youtube/docs/index.html" target="_blank">Easy YouTube</a>-link.</label><br />
-					<label title="Don't include links."><input type="radio" name="show_links" value="0" <?php if ((get_option('show_links')!=="1") && (get_option('show_links')!=="2")) echo "checked" ?> /> Don't add any links.</label>
-			</fieldset>
+				<fieldset>
+					<legend class="screen-reader-text"><span><?php _e("Show links?","wp-youtube-lyte") ?></span></legend>
+					<label title="Show YouTube-link"><input type="radio" name="show_links" value="1" <?php if (get_option('show_links')==="1") echo "checked" ?> /><?php _e(" Add YouTube-link.","wp-youtube-lyte") ?></label><br />
+					<label title="Show YouTube and Ease YouTube link"><input type="radio" name="show_links" value="2" <?php if (get_option('show_links')==="2") echo "checked" ?> /><?php _e(" Add both a YouTube and an <a href=\"http://icant.co.uk/easy-youtube/docs/index.html\" target=\"_blank\">Easy YouTube</a>-link.","wp-youtube-lyte") ?></label><br />
+					<label title="Don't include links."><input type="radio" name="show_links" value="0" <?php if ((get_option('show_links')!=="1") && (get_option('show_links')!=="2")) echo "checked" ?> /><?php _e(" Don't add any links.","wp-youtube-lyte") ?></label>
+				</fieldset>
 			</td>
          </tr>
+         <tr valign="top">
+                <th scope="row"><?php _e("Player position:","wp-youtube-lyte") ?></th>
+                <td>
+                        <fieldset>
+                                <legend class="screen-reader-text"><span>Left, center or right?</span></legend>
+                                <label title="Left"><input type="radio" name="position" value="0" <?php if (get_option('position','0')==="0") echo "checked" ?> /><?php _e("Left","wp-youtube-lyte") ?></label><br />
+				<label title="Center"><input type="radio" name="position" value="1" <?php if (get_option('position','0')==="1") echo "checked" ?> /><?php _e("Center","wp-youtube-lyte") ?></label>
+                        </fieldset>
+                </td>
+         </tr>
+         <tr valign="top">
+                <th scope="row"><?php _e("Play video in HD if possible?","wp-youtube-lyte") ?></th>
+                <td>
+                        <fieldset>
+                                <legend class="screen-reader-text"><span>HD or not?</span></legend>
+                                <label title="Enable HD?"><input type="radio" name="hidef" value="1" <?php if (get_option('hidef')==="1") echo "checked" ?> /><?php _e("Enable HD","wp-youtube-lyte") ?></label><br />
+                                <label title="Don't enable HD playback"><input type="radio" name="hidef" value="0" <?php if (get_option('hidef')!=="1") echo "checked" ?> /><?php _e("No HD, we're smallband!","wp-youtube-lyte") ?></label>
+                        </fieldset>
+                </td>
+	</tr>
 	 <tr valign="top">
-	 	<th scope="row">Bonus feature: <a href="http://blog.futtta.be/tag/donottrack" target="_blank">DoNotTrack</a></th>
+	 	<th scope="row"><?php _e("Bonus feature: ","wp-youtube-lyte") ?><a href="http://wordpress.org/extend/plugins/wp-donottrack/" target="_blank">DoNotTrack</a></th>
 		<td>
-			<fieldset>
-				<legend class="screen-reader-text"><span>Activate DoNotTrack</span></legend>
-				<label title="Enable DoNotTrack"><input type="radio" name="donottrack" value="1" <?php if (get_option('donottrack')==="1") echo "checked" ?> />Disable 3rd party tracking.</label><br />
-				<label title="Leave DoNotTrack disabled (default)"><input type="radio" name="donottrack" value="0" <?php if (get_option('donottrack')!=="1") echo "checked" ?> />I don't mind 3rd party tracking (default)</label>
-			</fieldset>
-		</td>
-	 </tr>
+			<?php _e("The DoNotTrack bonus feature has been removed from WP YouTube Lyte. If you need this functionality you might want to install <a href=\"http://wordpress.org/extend/plugins/wp-donottrack/\">WP DoNotTrack</a>, which offers a more flexible and powerful solution","wp-youtube-lyte") ?>
+
+               </td>
+        </tr>
     </table>
     
     <p class="submit">
@@ -99,11 +120,11 @@ function lyte_settings_page() {
 <div style="float:right;width:30%" id="lyte_admin_feed">
         <div style="margin-left:10px;margin-top:-5px;">
                 <h3>
-                        futtta about
+                        <?php _e("futtta about","wp-youtube-lyte") ?>
                         <select id="feed_dropdown" >
-                                <option value="1">WP YouTube Lyte</option>
-                                <option value="2">WordPress</option>
-                                <option value="3">Web Technology</option>
+                                <option value="1"><?php _e("WP YouTube Lyte","wp-youtube-lyte") ?></option>
+                                <option value="2"><?php _e("WordPress","wp-youtube-lyte") ?></option>
+                                <option value="3"><?php _e("Web Technology","wp-youtube-lyte") ?></option>
                         </select>
                 </h3>
                 <div id="futtta_feed"></div>
@@ -128,6 +149,7 @@ function lyte_settings_page() {
 
 	function show_feed(id) {
   		jQuery('#futtta_feed').rssfeed(feed[id], {
+			<?php if ( is_ssl() ) echo "ssl: true,"; ?>
     			limit: 4,
 			date: true,
 			header: false
